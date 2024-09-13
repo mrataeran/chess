1c4038bb0ad5d3e6db71c6ca9f146c696e4c08f4
mrataeran <mrataeran@gmail.com>
2024-09-12 19:18:32 -0600

    fixed index out of bounds error for the addPiece function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -21,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        board[position.getRow()][position.getColumn()] = piece;
+        board[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
